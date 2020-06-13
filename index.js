93ecb3e7e7ed7016d861784916662bb568af0f3c
Tanner McPhee <tannerm296@gmail.com>
2020-06-13 01:42:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string) {
+function logShout() {
   return ('HELLO')
 }
-console.log logShout(string)
+console.log logShout()
