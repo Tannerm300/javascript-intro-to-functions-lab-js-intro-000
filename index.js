af5c3e1348df9995dcee1c002a2ad7d7868afe64
Tanner McPhee <tannerm296@gmail.com>
2020-06-13 01:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout('Hello')) {
-  console.log (Hello.toUpperCase())
+function logShout('hello')) {
+  console.log (hello.toUpperCase())
+}
